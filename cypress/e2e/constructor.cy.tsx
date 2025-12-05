364e2d67641def7f5eabd9badb617ee5787c1899
Evgeniy Maslov <ruwell.masloves@yandex.ru>
2025-12-06 01:09:23 +0300

    feat: протестировано добавление ингредиентов из списка в конструктор

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,1 +1,49 @@
-// заготовка под будущие тесты
+describe('проверяем доступность приложения', function() {
+    it('сервис должен быть доступен по адресу localhost:4000', function() {
+        cy.visit('http://localhost:4000'); 
+    });
+});
+
+describe('проверяем страницу конструктора бургера', () => {
+    beforeEach(() => {
+        cy.intercept('GET', '**/api/ingredients**', {
+            fixture: 'ingredients.json'
+        }).as('getIngredients');
+
+        cy.visit('/');
+    });
+
+    it('проверяем добавление булки из списка в конструктор', () => {
+        cy.get('[data-testid="ingredient-item"]')
+            .eq(0)
+            .within(() => {
+                cy.get('button').click();
+            }
+        );
+        cy.get('[data-testid="no-bun-message"]').should('not.exist');
+        cy.get('[data-testid="constructor-bun-top"]').should('exist');
+        cy.get('[data-testid="constructor-bun-bottom"]').should('exist');
+    }),
+
+    it('проверяем добавление начинки из списка в конструктор', () => {
+        cy.get('[data-testid="ingredient-item"]')
+        .eq(1)
+        .within(() => {
+            cy.get('button').click();
+        });
+
+        cy.get('[data-testid="no-filling-message"]').should('not.exist');
+        cy.get('[data-testid="constructor-filling-item"]').should('exist');
+    }),
+
+    it('проверяем добавление соуса из списка в конструктор', () => {
+        cy.get('[data-testid="ingredient-item"]')
+        .eq(2)
+        .within(() => {
+            cy.get('button').click();
+        });
+
+        cy.get('[data-testid="no-filling-message"]').should('not.exist');
+        cy.get('[data-testid="constructor-filling-item"]').should('exist');
+    });
+})
